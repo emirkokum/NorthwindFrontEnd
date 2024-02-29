b7e4c4d509bb6a8bce02b367851cbd55c77d3f57
emir_ <emir_kokum_@hotmail.com>
2024-02-29 15:13:47 +0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,ProductComponent,CategoryComponent,NaviComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+ 
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = "Emir Kokum";
+
 }
